bc9dcbf328a7150d9193bfcaed3980645f2e40b1
azure <rz7@outlook.jp>
2019-10-30 21:18:39 +0900

    Implement minimize to tray

--- WiinUPro/Windows/MainWindow.xaml.cs
+++ WiinUPro/Windows/MainWindow.xaml.cs
@@ -37,8 +37,58 @@ namespace WiinUPro
 
             WinBtStream.OverrideSharingMode = true;
             WinBtStream.OverridenFileShare = System.IO.FileShare.ReadWrite;
+
+            TrayIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath),
+                Visible = true
+            };
+
+            Closed += (s, e) => TrayIcon.Dispose();
+
+            var contextMenu = new System.Windows.Forms.ContextMenu();
+            contextMenu.MenuItems.Add("E&xit", (s, e) => Close());
+            TrayIcon.ContextMenu = contextMenu;
+
+            TrayIcon.DoubleClick += (s, e) =>
+            {
+                if (IsEnabled)
+                {
+                    MinimizeToTray();
+                }
+                else
+                {
+                    Restore();
+                }
+            };
         }
-        
+
+        System.Windows.Forms.NotifyIcon TrayIcon { get; }
+
+        void MinimizeToTray()
+        {
+            IsEnabled = false;
+            Hide();
+            ShowInTaskbar = false;
+        }
+
+        void Restore()
+        {
+            IsEnabled = true;
+            Show();
+            WindowState = WindowState.Normal;
+            ShowInTaskbar = true;
+        }
+
+        protected override void OnStateChanged(System.EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                MinimizeToTray();
+            }
+            base.OnStateChanged(e);
+        }
+
         public void Refresh()
         {
             // Prevent this method from being spammed
